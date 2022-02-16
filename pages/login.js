bbcc2291d265b56569ab609eb4035a3c1bf2b693
Connor Claxton <czclaxton@gmail.com>
2022-02-16 00:37:41 -0500

    formatting updates again

--- pages/login.js
+++ pages/login.js
@@ -3,13 +3,13 @@ import { getProviders, signIn } from 'next-auth/react'
 
 function Login({ providers }) {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-900 ">
-      <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />
+    <div className='flex min-h-screen w-full flex-col items-center justify-center bg-gray-900 '>
+      <img className='mb-5 w-52' src='https://links.papareact.com/9xl' alt='' />
 
-      {Object.values(providers).map((provider) => (
+      {Object.values(providers).map(provider => (
         <div key={provider.name}>
           <button
-            className="rounded-full bg-[#18D860] p-5 text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-cyan-500"
+            className='rounded-full bg-[#18D860] p-5 text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-cyan-500'
             onClick={() => signIn(provider.id, { callbackUrl: '/' })}
           >
             Login with {provider.name}
