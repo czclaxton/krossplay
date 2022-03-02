08218d428eddf410c20232c1447d92938b0f282d
Connor Claxton <czclaxton@gmail.com>
2022-03-02 13:16:42 -0500

    updated log

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,7 @@ const refreshAccessToken = async token => {
     spotifyApi.setRefreshToken(token.refreshToken)
 
     const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
-    console.log(`Refreshed Token: ${refreshedToken.access_token}`)
+    console.log(`Refreshed Token`)
 
     return {
       ...token,
