04b16a21f22b050324bb9d1fac5f9516dbb9dc4b
Connor Claxton <czclaxton@gmail.com>
2022-03-07 19:28:08 -0500

    installed sequelize dependencies

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import SpotifyProvider from 'next-auth/providers/spotify'
 import { spotifyApi, LOGIN_URL } from '../../../lib/spotify'
+import SequelizeAdapter from '@next-auth/sequelize-adapter'
+import { Sequelize } from 'sequelize'
 
 const refreshAccessToken = async token => {
   try {
